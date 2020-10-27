e04d8005102bfea3094d2dc5864dfeb79f780657
Christian Liebel <christian.liebel@thinktecture.com>
2020-10-27 16:52:01 +0100

    Lab #12

--- src/app.js
+++ src/app.js
@@ -34,6 +34,7 @@ const fileOptions = {
 }
 
 const btnSave = document.querySelector('#save');
+btnSave.disabled = !('showSaveFilePicker' in window);
 btnSave.addEventListener('click', async () => {
     const blob = await toBlob(canvas);
     const handle = await window.showSaveFilePicker(fileOptions);
@@ -43,6 +44,7 @@ btnSave.addEventListener('click', async () => {
 });
 
 const btnOpen = document.querySelector('#open');
+btnOpen.disabled = !('showOpenFilePicker' in window);
 btnOpen.addEventListener('click', async () => {
     const [handle] = await window.showOpenFilePicker(fileOptions);
     const file = await handle.getFile();
@@ -51,6 +53,7 @@ btnOpen.addEventListener('click', async () => {
 });
 
 const btnCopy = document.querySelector('#copy');
+btnCopy.disabled = !('write' in navigator.clipboard);
 btnCopy.addEventListener('click', async () => {
     const blob = await toBlob(canvas);
     await navigator.clipboard.write([
@@ -59,6 +62,7 @@ btnCopy.addEventListener('click', async () => {
 });
 
 const btnPaste = document.querySelector('#paste');
+btnPaste.disabled = !('read' in navigator.clipboard);
 btnPaste.addEventListener('click', async () => {
     const clipboardItems = await navigator.clipboard.read();
     for (const clipboardItem of clipboardItems) {
